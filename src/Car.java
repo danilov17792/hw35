f5d09943f8b7c919aa0de2401e5fdc59487006cc
Vladimir Danilov <danilov17792@yandex.ru>
2020-04-15 00:01:50 +0300

    hw35

--- src/Car.java
+++ src/Car.java
@@ -1,34 +1,51 @@
 public class Car implements Runnable {
     private static int CARS_COUNT;
+
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
+
+    public Car(int speed) {
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public void addToRace(Race race) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            race.getStartSignal().await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            race.setWinner(this);
+            race.getFinishSignal().await();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+        } finally {
+            Thread.currentThread().interrupt();
         }
     }
 }
